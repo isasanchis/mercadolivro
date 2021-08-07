6d022626c83a2171b63769f248be182bae10b0e6
Isabella <isasanchis3@gmail.com>
2021-08-07 20:21:48 -0300

    create book and customer response

--- src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
@@ -4,10 +4,13 @@ import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.controller.response.BookResponse
+import com.mercadolivro.controller.response.CustomerResponse
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.enums.CustomerStatus
+import com.mercadolivro.repository.CustomerRepository
 
 fun PostCustomerRequest.toCustomerModel(): CustomerModel {
     return CustomerModel(name = this.name, email = this.email, status = CustomerStatus.ATIVO)
@@ -35,3 +38,26 @@ fun PutBookRequest.toBookModel(previousValue: BookModel): BookModel {
         customer = previousValue.customer
     )
 }
+
+fun CustomerModel.toResponse(): CustomerResponse {
+    return CustomerResponse(
+        id = this.id,
+        name = this.name,
+        email = this.email,
+        status = this.status
+    )
+}
+
+fun BookModel.toResponse(): BookResponse {
+    return BookResponse(
+        id = this.id,
+        name = this.name,
+        price = this.price,
+        customer = this.customer,
+        status = this.status
+    )
+}
+
+
+
+
